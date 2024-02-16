bf8dc361dc365dac0aedf1d90efa4cdbeab3132f
Paulina Bastista <uwupaulina09@gmail.com>
2024-02-15 19:12:36 -0700

    Se agrega el proyecto

---
+++ src/componentes/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import pokelogo from './images/Logo-Pokemon-500x313,png; 
+
+const Logo = () => {
+    return (
+        <a href="/">
+            <img src={pokelogo} alt="Logo" /> {}
+        </a>
+    );
+};
+
+export default Logo;
